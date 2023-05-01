e41e802e0a0a775961e07077ecc3364b79c7f402
OtakuGod0 <130218032+OtakuGod0@users.noreply.github.com>
2023-05-01 10:01:19 +0000

    day 1

--- test.c
+++ test.c
@@ -21,20 +21,20 @@ int main(){
     printf("The area of the circle is %f\n", pi*ra*ra);
     
     //WAP to find the area of a rectangle
-    int l, b; 
+    int l, br; 
     printf("Input length and breath in order ");
-    scanf("%d %d", &l, &b);
-    printf("Area of the rectangle is %d", l*b);
+    scanf("%d %d", &l, &br);
+    printf("Area of the rectangle is %d", l*br);
 
     //WAP to input seconds and convert into hours, minutes and seconds
-    int seconds = 1000, minutes, hours; 
+    int seconds = 17403, minutes, hours; 
     hours = seconds / (60*60);
     seconds = seconds  - hours * 60 * 60;
 
     minutes = seconds / 60;
     seconds = seconds - minutes * 60;
 
-    printf("%d hours %d minutes %d second", hours, minutes, seconds);
+    printf("%d hours %d minutes %d second \n", hours, minutes, seconds);
 
     return 0;
 }
